cff8aa50a9b3d9257bf9866b63fe5cdd3b802398
labelwithshipiroo <info@shipiroo.com>
2025-12-14 13:18:48 +0100

    Remove unwanted addon folders from repository

--- oldaddonsdonot toucj/customer_segment/models/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-logging.getLogger('customer_segment').info("Customer Segment module loaded")
-
-from . import customer_segment, res_partner
--- oldaddonsdonot toucj/customer_segment/models/customer_segment.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class CustomerSegment(models.Model):
-    _name = 'customer.segment'
-    _description = 'Customer Segment'
-    name = fields.Char(string='Name', required=True)
-    description = fields.Text(string='Description')
