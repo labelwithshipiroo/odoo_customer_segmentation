845ef68bfd2177b2f6765372a038f1b6277bfab9
labelwithshipiroo <info@shipiroo.com>
2025-12-14 17:30:39 +0100

    Extend odoo_addon to add KPI tabs to products, invoices, and quotations

--- addons/odoo_addon/__manifest__.py
+++ addons/odoo_addon/__manifest__.py
@@ -7,9 +7,15 @@
     'depends': [
         'base',
         'contacts',
+        'product',
+        'account',
+        'sale',
     ],
     'data': [
         'views/res_partner_views.xml',
+        'views/product_views.xml',
+        'views/account_move_views.xml',
+        'views/sale_order_views.xml',
     ],
     'installable': True,
     'application': False,
