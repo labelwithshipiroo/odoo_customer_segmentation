354a3cbfc993c7075d112ecd032832ea52b36e09
labelwithshipiroo <info@shipiroo.com>
2025-12-14 16:49:41 +0100

    Add odoo_addon: Contacts KPI and Targets tab extension

---
+++ addons/odoo_addon/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Contacts KPI and Targets',
+    'version': '1.0.0',
+    'category': 'CRM',
+    'author': 'YannickCuvelie',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+        'contacts',
+    ],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
