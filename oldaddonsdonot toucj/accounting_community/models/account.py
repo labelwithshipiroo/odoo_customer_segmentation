c292dd4f6ec0748d98491baca5436379f488a177
labelwithshipiroo <info@shipiroo.com>
2025-12-14 13:19:37 +0100

    Complete repository cleanup: remove all unwanted addon folders, keep only odoo_kpi and accounting_community

---
+++ oldaddonsdonot toucj/accounting_community/models/account.py
@@ -0,0 +1,12 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class AccountAccount(models.Model):
+    _inherit = "account.account"
+
+    centralized = fields.Boolean(
+        help="If flagged, no details will be displayed in "
+        "the General Ledger report, "
+        "only centralized amounts per period.",
+    )
