761dd32c726a35009002dcfeabd3b36ec076b8e6
labelwithshipiroo <info@shipiroo.com>
2025-12-14 15:14:04 +0100

    Add KPI tab to product form view with dynamic KPI values display

--- addons/odoo_kpi/__manifest__.py
+++ addons/odoo_kpi/__manifest__.py
@@ -30,6 +30,7 @@
         'views/kpi_target_views.xml',
         'views/kpi_dashboard_views.xml',
         'views/kpi_widget_views.xml',
+        'views/product_views.xml',
         'data/kpi_data.xml',
     ],
     'images': ['static/description/icon.png'],
