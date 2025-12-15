97a2d4c013de04b1904f53890d7d6ee85d37506a
labelwithshipiroo <info@shipiroo.com>
2025-12-15 15:43:05 +0100

    Implement whiteboard component with drag and drop functionality

--- addons/odoo_board/static/src/odoo_board/odoo_board.js
+++ addons/odoo_board/static/src/odoo_board/odoo_board.js
@@ -1,10 +1,120 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState, useRef, onMounted } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { WhiteboardElement } from "./whiteboard_element";
 
 class OdooBoard extends Component {
-    static template = "odoo_board";
+    static template = "whiteboard.Board";
+    static components = { WhiteboardElement };
+
+    setup() {
+        this.state = useState({
+            elements: [],
+            selectedTool: 'select',
+            selectedElement: null,
+            isDragging: false,
+            dragOffset: { x: 0, y: 0 }
+        });
+
+        this.canvasRef = useRef("canvas");
+        this.colors = ['#fef3c7', '#dbeafe', '#dcfce7', '#fce7f3', '#fed7d7', '#e0e7ff'];
+
+        onMounted(() => {
+            this.setupCanvas();
+        });
+    }
+
+    setupCanvas() {
+        const canvas = this.canvasRef.el;
+        if (!canvas) return;
+
+        canvas.addEventListener('mousedown', this.onCanvasMouseDown.bind(this));
+        canvas.addEventListener('mousemove', this.onCanvasMouseMove.bind(this));
+        canvas.addEventListener('mouseup', this.onCanvasMouseUp.bind(this));
+    }
+
+    selectTool(tool) {
+        this.state.selectedTool = tool;
+        this.state.selectedElement = null;
+    }
+
+    addElement(type) {
+        const newElement = {
+            id: Date.now(),
+            type: type,
+            content: type === 'sticky' ? 'New sticky note' : 'New text',
+            x: 100,
+            y: 100,
+            width: type === 'sticky' ? 200 : 300,
+            height: type === 'sticky' ? 150 : 100,
+            color: this.colors[0],
+            fontSize: 14
+        };
+        this.state.elements.push(newElement);
+    }
+
+    onSelectElement(elementId) {
+        this.state.selectedElement = elementId;
+    }
+
+    deleteElement() {
+        if (this.state.selectedElement) {
+            this.state.elements = this.state.elements.filter(el => el.id !== this.state.selectedElement);
+            this.state.selectedElement = null;
+        }
+    }
+
+    changeColor(color) {
+        if (this.state.selectedElement) {
+            const element = this.state.elements.find(el => el.id === this.state.selectedElement);
+            if (element) {
+                element.color = color;
+            }
+        }
+    }
+
+    onDragStart(elementId, event) {
+        this.state.isDragging = true;
+        this.state.selectedElement = elementId;
+        const element = this.state.elements.find(el => el.id === elementId);
+        if (element) {
+            this.state.dragOffset.x = event.clientX - element.x;
+            this.state.dragOffset.y = event.clientY - element.y;
+        }
+    }
+
+    onCanvasMouseDown(event) {
+        if (this.state.selectedTool === 'select') {
+            this.state.selectedElement = null;
+        }
+    }
+
+    onCanvasMouseMove(event) {
+        if (this.state.isDragging && this.state.selectedElement) {
+            const element = this.state.elements.find(el => el.id === this.state.selectedElement);
+            if (element) {
+                element.x = event.clientX - this.state.dragOffset.x;
+                element.y = event.clientY - this.state.dragOffset.y;
+            }
+        }
+    }
+
+    onCanvasMouseUp() {
+        this.state.isDragging = false;
+    }
+
+    onUpdateContent(elementId, content) {
+        const element = this.state.elements.find(el => el.id === elementId);
+        if (element) {
+            element.content = content;
+        }
+    }
+
+    saveElements() {
+        // TODO: Save to backend
+        console.log('Saving elements:', this.state.elements);
+    }
 }
 
 registry.category("lazy_components").add("OdooBoard", OdooBoard);
---
+++ addons/odoo_board/static/src/odoo_board/whiteboard_element.js
@@ -0,0 +1,31 @@
+/** @odoo-module **/
+
+import { Component, useState } from "@odoo/owl";
+
+class WhiteboardElement extends Component {
+    static template = "whiteboard.Element";
+
+    setup() {
+        this.state = useState({
+            isEditing: false
+        });
+    }
+
+    get isSelected() {
+        return this.props.selectedId === this.props.element.id;
+    }
+
+    onMouseDown(event) {
+        event.stopPropagation();
+        this.props.onSelect(this.props.element.id);
+        if (this.state.isEditing) return;
+
+        this.props.onDragStart(this.props.element.id, event);
+    }
+
+    onTextChange(event) {
+        this.props.onUpdateContent(this.props.element.id, event.target.value);
+    }
+}
+
+export { WhiteboardElement };
