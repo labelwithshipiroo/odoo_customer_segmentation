2221bb5d516b2f85183dec8c4495e37ff26a4325
labelwithshipiroo <info@shipiroo.com>
2025-12-15 14:11:05 +0100

    Declare jsonschema external python dependency

--- addons/quickboard/__manifest__.py
+++ addons/quickboard/__manifest__.py
@@ -9,6 +9,7 @@
     'category': 'Productivity',
     'version': '18.0.1.0.0',
     'depends': ['web'],
+    'external_dependencies': {'python': ['jsonschema']},
     'data': [
         'security/quickboard_security.xml',
         'security/ir.model.access.csv',
