c24414f6de1e7782820318cf87603d83c26690f5
labelwithshipiroo <info@shipiroo.com>
2025-12-15 10:25:01 +0100

    Revert to completely blank page without Odoo navigation

--- addons/odoo_addon/controllers/test_page.py
+++ addons/odoo_addon/controllers/test_page.py
@@ -6,4 +6,28 @@ class TestPageController(http.Controller):
 
     @http.route('/odoo_addon/test_page', type='http', auth='user', website=False)
     def test_page(self, **kwargs):
-        return request.render('odoo_addon.test_page_template', {})
+        return """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Test Page</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    min-height: 100vh;
+                    margin: 0;
+                    background: white;
+                }
+                h1 {
+                    color: #333;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Hello</h1>
+        </body>
+        </html>
+        """
