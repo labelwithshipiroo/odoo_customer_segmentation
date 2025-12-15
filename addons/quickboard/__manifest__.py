8c8a0fc2d1feb12aeb4b058eeea8822b72094c10
labelwithshipiroo <info@shipiroo.com>
2025-12-15 14:08:54 +0100

    Fix manifest version for Odoo 18 compatibility

--- addons/quickboard/__manifest__.py
+++ addons/quickboard/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     'author': "Yoni Tjio",
     'category': 'Productivity',
-    'version': '17.0.1.0.0',
+    'version': '18.0.1.0.0',
     'depends': ['web'],
     'data': [
         'security/quickboard_security.xml',
