891221f8430e3ab90d878e93b0795deea8a5a3a0
labelwithshipiroo <info@shipiroo.com>
2025-12-15 12:02:07 +0100

    add debug prints in write method

--- addons/odoo_addon/models/account_extension.py
+++ addons/odoo_addon/models/account_extension.py
@@ -8,16 +8,20 @@ class AccountAccount(models.Model):
     x_api_mapping = fields.Many2one('unified.account', string="API Mapping")
 
     def write(self, vals):
+        print('write called with vals:', vals)
         res = super(AccountAccount, self).write(vals)
         if 'x_api_mapping' in vals and vals['x_api_mapping']:
+            print('x_api_mapping changed to:', vals['x_api_mapping'])
             unified = self.env['unified.account'].browse(vals['x_api_mapping'])
             if unified:
+                print('calling API with unified_id:', unified.api_id)
                 try:
-                    requests.post('https://192.168.0.212:3002/odoo/accounts/map', json={
+                    response = requests.post('https://192.168.0.212:3002/odoo/accounts/map', json={
                         'account_id': self.id,
                         'unified_id': unified.api_id,
                         'account_code': self.code
                     }, timeout=5)
-                except:
-                    pass  # Ignore errors
+                    print('API response:', response.status_code, response.text)
+                except Exception as e:
+                    print('API call failed:', e)
         return res
